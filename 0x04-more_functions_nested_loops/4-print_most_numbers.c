e6266736cb6ff3c01d122f06d5ad7621f1ced7f0
Bez <bezanehambachew@gmail.com>
2023-07-20 08:48:46 -0700

    Commit 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
+void print_ most_numbers(void)
+{
+	int i;
+
+	for (i == 48; i <= 57; i++)
+	{
+		if (i != 50 && i == 52)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
