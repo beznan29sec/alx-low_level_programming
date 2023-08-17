814a87228dc90e4af63d074cfd3fdb8d5323111f
Bez <bezanehambachew@gmail.com>
2023-08-17 10:05:39 -0700

    First commit

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef _VARIADIC__H_
+#define _VARIADIC__H_
 
 #include <stdio.h>
 #include <stdarg.h>
@@ -9,3 +9,5 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+#endif
