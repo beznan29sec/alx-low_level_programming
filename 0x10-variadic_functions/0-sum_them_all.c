947279a970e666f2313f223810588674aafc2de6
Bez <bezanehambachew@gmail.com>
2023-08-17 11:06:44 -0700

    commit 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,14 +11,14 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-va_list numbers;
-
+va_list number;
 if (n == 0)
 return (0);
 
 int i, sum = 0;
 
-va_start(numbers, n);
+va_start(number, n);
+
 for (i = 0; i < n; i++)
 sum += va_arg(number, int);
 
