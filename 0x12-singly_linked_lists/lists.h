8d91c8dc84af2583ea615dd59cda69b8fdca96ab
Bez <bezanehambachew@gmail.com>
2023-08-24 14:18:54 -0700

    Commit 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,18 +1,23 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdio.h>
-#include <stdlib.h>
 
-int _putchar(char c);
-size_t print_list(const list_t *h);
 /**
- * struct- Liked list
- * @val: value of the node
- * @link: points to the next node
+ * struct list_s - linked list
+ * @str: string
+ * @len: length of the string
+ * @next: points to the next node
  */
-struct node
+typedef struct list_s
 {
-	int val;
-	struct node *link;
-};
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
 #endif
