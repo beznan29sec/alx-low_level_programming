2cb025f011c1eaa081a66ad25d9747b04acfcee5
beznan29sec <bezanehambachew@gmail.com>
2023-08-29 12:40:58 -0700

    Commit 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,11 +9,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *add;
 
-	add = (listint_t)malloc(sizeof(listint_t);
+	add = malloc(sizeof(listint_t));
 		if (!add)
 			return (NULL);
-
-		add->n = *head;
+		add->n = n;
+		add->next = *head;
 		*head = add;
 	return (add);
 }
