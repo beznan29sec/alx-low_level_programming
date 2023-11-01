f086dc97d6b1edc44c941ad916db74c919b08275
beznan29sec <bezanehambachew@gmail.com>
2023-11-01 01:12:31 -0700

    Commit 0 and 1

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - a function that create array
+ * @size: size of array
+ * @c: char
+ * Returns NULL if size = 0 or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
+
+	s = malloc(sizeof(char) * size);
+	if (size == 0 || s == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		s[i] = c;
+		return (s);
+}
+
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - a function that duplicate to new memory
+ * @str: char
+ * Return: NULL if str = NULL
+ */
+char *_strdup(char *str)
+{
+	char *a;
+	int i;
+	int n = 0;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	a = malloc(sizeof(char) * (i + 1));
+	if (a == NULL)
+		return (NULL);
+	for (n = 0; str[n]; n++)
+		a[n] = str[n];
+		return (a);
+}
